6bba2e35f4d472ccf4e7277b0438d4e3c06e902b
visualstudio <visualstudio@122-PC27>
2019-02-06 10:08:22 -0500

    jone

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,12 +4,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace peanut
 {
-    class Program
+    class inder
     {
+        static int i = 0;
         static void Main(string[] args)
         {
+            MethodA();
+            Console.ReadLine();
+            
+        }
+
+        public static void MethodA()
+        {
+            while (Peanut())
+            {
+
+                Console.WriteLine("i is {0}", i);
+
+                if (i>10)
+                    {
+                    return;
+                }
+            }
+        }
+        public static bool Peanut()
+        {
+            i++;
+            return true;
         }
     }
 }
+       
+    
+
